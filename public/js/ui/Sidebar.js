f3c9ee1a0142c797fa8410acdbd762592bd6aade
Igor Koliberskiy <kolibry60rus@mail.ru>
2023-01-26 23:40:46 +0300

    HW bhj-diploma

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,10 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
-
+    document.querySelector(".sidebar-toggle").addEventListener('click', () => {
+      document.body.classList.toggle('sidebar-open');
+      document.body.classList.toggle('sidebar-collapse');
+    });
   }
 
   /**
@@ -29,6 +32,19 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
-
+    document.querySelector('.menu-item_login > a').onclick = event => {
+      event.preventDefault();
+      App.getModal('login').open();
+    };
+    document.querySelector('.menu-item_register > a').onclick = event => {
+      event.preventDefault();
+      App.getModal('register').open();
+    };
+    document.querySelector('.menu-item_logout > a').onclick = event => {
+      event.preventDefault();
+      User.logout((err, response) => {
+        if (response && response.success) App.setState('init');
+      });
+    };
   }
 }
